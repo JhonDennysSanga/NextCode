5b86457bad6e89288c665bc7dac109770eac9eb7
JhonDennysSanga <jhodsang@espol.edu.ec>
2020-10-03 12:54:16 -0500

    Nueva Rama Jhon

--- app/src/main/java/com/example/nextcode/MainActivity.java
+++ app/src/main/java/com/example/nextcode/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //MainActivity
+    //ramaJhon
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
